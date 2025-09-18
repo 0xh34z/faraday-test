9f3c92bbdf298c5f56dd39f73bad6de28371c50c
Kasper <kaspervandekimmenade@proton.me>
2025-09-18 12:11:52 +0200

    .

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ping -c 1 1.1.1.1 > /dev/null
+while true; do
+  ping -c 1 1.1.1.1 > /dev/null
 
-if [ $? -eq 0 ]; then
-  mpv --no-terminal --quiet beep.mp3
-fi
+  if [ $? -eq 0 ]; then
+    mpv --no-terminal --quiet beep.mp3
+  fi
+
+#pauze tijdens pingen, pas aan naar wens
+  sleep 1
+done
